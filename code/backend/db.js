300901a45ed871bd137c6c64e12db3b6b6535b74
IshanFernando <e18098@eng.pdn.ac.lk>
2022-11-26 15:24:29 +0530

    Add db connected log

--- code/backend/db.js
+++ code/backend/db.js
@@ -4,6 +4,9 @@ function connectDB() {
     mongoose.connect(process.env.MONGO_URL);
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    db.on("connected", () => {
+        console.log("MongoDB Connected");
+    });
 }
 
 module.exports = { connectDB };
