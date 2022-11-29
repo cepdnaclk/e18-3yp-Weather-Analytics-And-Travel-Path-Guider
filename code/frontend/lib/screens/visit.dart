0208576ebc0be63e7ecb3d84eddb3b83810257a0
NipunFernando <e18100@eng.pdn.ac.lk>
2022-11-30 05:06:13 +0530

    visit update

--- code/frontend/lib/screens/visit.dart
+++ code/frontend/lib/screens/visit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:WhetherTravel/screens/welcome.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'package:to_app/ui/size_config.dart';
+//import 'package:to_app/ui/size_config.dart';
 
 
 import 'package:flutter/cupertino.dart';
@@ -21,7 +21,7 @@ class Visit extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ScreenConfig.init();
+    //ScreenConfig.init();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Whether Travel',
@@ -29,198 +29,38 @@ class Visit extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Scaffold(
-        backgroundColor: Color(0xff121421),
-        body: SafeArea(
-        child: ListView(
-        physics: BouncingScrollPhysics(),
-        children: [
-        Padding(
-        padding: EdgeInsets.only(
-        left: 28.w,
-        right: 18.w,
-        top: 36.h,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child : Text("Whether Travel"),
+          ) ,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          actions: <Widget> [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
+          flexibleSpace: Image.asset(
+            "assets/bg.png",
+            fit: BoxFit.cover,
+          ),
         ),
-        child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-        Text("Discover",
-        style: TextStyle(
-        color: Colors.white,
-        fontSize: 34.w,
-        fontWeight: FontWeight.bold)),
-        InkWell(
-    borderRadius: BorderRadius.circular(360),
-    onTap: onSearchIconTapped,
-    child: Container(
-    height: 35.w,
-    width: 35.w,
-    child: Center(
-    child: SvgAsset(
-    assetName: AssetName.search,
-    height: 24.w,
-    width: 24.w,
-    ),
-    ),
-    ),
-    ),
-    ],
-    ),
-    ),
-    Container(
-    height: 120.h,
-    child: ListView(
-    physics: BouncingScrollPhysics(),
-    scrollDirection: Axis.horizontal,
-    children: [
-    SizedBox(
-    width: 28.w,
-    ),
-    CategoryBoxes(
-    text: "Insomnia",
-    onPressed: (value) => print(value),
-    ),
-    CategoryBoxes(
-    text: "Depression",
-    onPressed: (value) => print(value),
-    ),
-    CategoryBoxes(
-    text: "Baby Sleep",
-    onPressed: (value) => print(value),
-    ),
-    CategoryBoxes(
-    text: "Insomnia",
-    onPressed: (value) => print(value),
-    ),
-    ],
-    ),
-    ),
-    Padding(
-    padding: EdgeInsets.symmetric(horizontal: 28.w),
-    child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-    Text(
-    "Recommended",
-    style: TextStyle(
-    color: Color(0xff515979),
-    fontWeight: FontWeight.w500,
-    fontSize: 14.w),
-    ),
-    GestureDetector(
-    onTap: onSeeAllTapped,
-    child: Text("See All",
-    style: TextStyle(
-    color: Color(0xff4A80F0),
-    fontWeight: FontWeight.w500,
-    fontSize: 14.w)))
-    ],
-    ),
-    ),
-    SizedBox(
-    height: 16.h,
-    ),
-    SizedBox(
-    height: 176.w,
-    child: ListView(
-    physics: BouncingScrollPhysics(),
-    scrollDirection: Axis.horizontal,
-    children: [
-    SizedBox(width: 28.w),
-    DiscoverCard(
-    tag: "sleepMeditation",
-    onTap: onSleepMeditationTapped,
-    title: "Sleep Meditation",
-    subtitle: "7 Day Audio and Video Series",
-    ),
-    SizedBox(width: 20.w),
-    DiscoverCard(
-    onTap: onDepressionHealingTapped,
-    title: "Depression Healing",
-    subtitle: "10 Days Audio and Video Series",
-    gradientStartColor: Color(0xffFC67A7),
-    gradientEndColor: Color(0xffF6815B),
-    ),
-    ],
-    ),
-    ),
-    SizedBox(height: 28.h),
-    Padding(
-    padding: EdgeInsets.only(left: 28.w),
-    child: Text(
-    "Recent",
-    style: TextStyle(
-    color: Color(0xff515979),
-    fontWeight: FontWeight.w500,
-    fontSize: 14.w),
-    ),
-    ),
-    SizedBox(height: 16.h),
-    Padding(
-    padding:  EdgeInsets.symmetric(horizontal: 28.w),
-    child: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 19.w, mainAxisExtent:  125.w, mainAxisSpacing: 19.w),
-    shrinkWrap: true,
-    physics: NeverScrollableScrollPhysics(),
-    children: [
-    DiscoverSmallCard(
-    onTap: (){},
-    title: "Calming Sounds",
-    gradientStartColor: Color(0xff13DEA0),
-    gradientEndColor: Color(0xff06B782),
-    ),
-    DiscoverSmallCard(
-    onTap: (){},
-    title: "Insomnia",
-    gradientStartColor: Color(0xffFC67A7),
-    gradientEndColor: Color(0xffF6815B),
-    icon:    SvgAsset(
-    assetName: AssetName.tape,
-    height: 24.w,
-    width: 24.w,
-    ),
-    ),
-    DiscoverSmallCard(
-    onTap: (){
-    },
-    title: "For Children",
-    gradientStartColor: Color(0xffFFD541),
-    gradientEndColor: Color(0xffF0B31A),
-    ),
-    DiscoverSmallCard(
-    onTap: (){},
-    title: "Tips For Sleeping",
-    icon:  SvgAsset(
-    assetName: AssetName.tape,
-    height: 24.w,
-    width: 24.w,
-    ),
-    ),
-    ],
-
-    ),
-    )
-    ],
-    ),
-    ),
+        body : Center(
+          child : Text(
+            "Weather Analytics and Travel Path Guider",
+            style: TextStyle(fontSize: 30.0),
+          ),
         ),
+      ),
     );
   }
-}
-
-
-void onSeeAllTapped() {
-  Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
-}
-
-void onSleepMeditationTapped() {
-  Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
-}
-
-void onDepressionHealingTapped() {
-  Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
-}
-
-void onSearchIconTapped() {
-  Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
 }
 
