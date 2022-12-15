b1cd2fff71465dbf7a0ce3dde833f3d55d3f8e9a
NipunFernando <e18100@eng.pdn.ac.lk>
2022-12-16 00:56:33 +0530

    All good, before google sign in

--- code/newFrontend/lib/main.dart
+++ code/newFrontend/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'navigation_home_screen.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:best_flutter_ui_templates/delayed_animation.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +15,61 @@ void main() async {
   ]).then((_) => runApp(MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+// ###################################################
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+  late double _scale;
+  late AnimationController _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//       statusBarColor: Colors.transparent,
+//       statusBarIconBrightness: Brightness.dark,
+//       statusBarBrightness:
+//           !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
+//       systemNavigationBarColor: Colors.white,
+//       systemNavigationBarDividerColor: Colors.transparent,
+//       systemNavigationBarIconBrightness: Brightness.dark,
+//     ));
+//     return MaterialApp(
+//       title: 'Flutter UI',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         textTheme: AppTheme.textTheme,
+//         platform: TargetPlatform.iOS,
+//       ),
+//       //home: NavigationHomeScreen(),
+//     );
+//   }
+// }
+
+
+// ############################################################
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -25,18 +81,157 @@ class MyApp extends StatelessWidget {
       systemNavigationBarDividerColor: Colors.transparent,
       systemNavigationBarIconBrightness: Brightness.dark,
     ));
+    final color = Colors.white;
+    _scale = 1 - _controller.value;
     return MaterialApp(
-      title: 'Flutter UI',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: AppTheme.textTheme,
-        platform: TargetPlatform.iOS,
-      ),
-      home: NavigationHomeScreen(),
+      home: Scaffold(
+          backgroundColor: Color(0xFF8185E2),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 90.0,
+                ),
+                AvatarGlow(
+                  endRadius: 100,
+                  duration: Duration(seconds: 1),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 1),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.asset('assets/images/logo_big.png'),
+                        radius: 90.0,
+                      )),
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "WhetherTravel",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                //  SizedBox(
+                //   height: 30.0,
+                // ),
+                // DelayedAnimation(
+                //   child: Text(
+                //     "",
+                //     style: TextStyle(
+                //         fontWeight: FontWeight.bold,
+                //         fontSize: 35.0,
+                //         color: color),
+                //   ),
+                //   delay: delayedAmount + 2000,
+                // ),
+                // SizedBox(
+                //   height: 30.0,
+                // ),
+                DelayedAnimation(
+                  child: Text(
+                    "Weather Analytics and Travel Path Guider",
+                    style: TextStyle(fontSize: 15.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "",
+                    style: TextStyle(fontSize: 15.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "",
+                    style: TextStyle(fontSize: 15.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                DelayedAnimation(
+                  child: GestureDetector(
+                    onTapDown: _onTapDown,
+                    onTapUp: _onTapUp,
+                    child: Transform.scale(
+                      scale: _scale,
+                      child: _animatedButtonUI,
+                    ),
+                  ),
+                  delay: delayedAmount + 4000,
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Visit Website".toUpperCase(),
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 5000,
+                ),
+              ],
+            ),
+          )
+          //  Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: <Widget>[
+          //     Text('Tap on the Below Button',style: TextStyle(color: Colors.grey[400],fontSize: 20.0),),
+          //     SizedBox(
+          //       height: 20.0,
+          //     ),
+          //      Center(
+
+          //   ),
+          //   ],
+
+          // ),
+          ),
     );
   }
+
+  Widget get _animatedButtonUI => Container(
+        height: 60,
+        width: 270,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Colors.white,
+        ),
+        
+        child: Center(
+          child: Text(
+            'LOGIN WITH GOOGLE',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF8185E2),
+            ),
+          ),
+        ),
+      );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
 }
+//#########################################################################
 
 class HexColor extends Color {
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
