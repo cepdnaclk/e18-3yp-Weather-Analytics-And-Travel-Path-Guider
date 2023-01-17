e9b47f7ca97ac0e63956610822ccdafdd4790077
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-01-17 14:48:04 +0530

    123

--- code/backend/routes/v1/emergency.js
+++ code/backend/routes/v1/emergency.js
@@ -3,9 +3,7 @@ const { EmergencyButtonPress } = require("../../models/EmergencyButtonPress");
 
 
 router.get('/lastEmergencyButtonPress', async (req, res) => {
-    res.json(await EmergencyButtonPress.find({
-        location: req.params.location
-    }).sort({ _id: -1 }).limit(1));
+    res.json(await EmergencyButtonPress.find().sort({ _id: -1 }).limit(1));
 })
 
 
